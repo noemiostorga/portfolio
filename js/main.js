accc31aea995aef758154b12e3814cdf00b04b02
Noemi Ostorga <noemiostorga@gmail.com>
2016-09-26 16:40:23 -0700

    added chinesewar image

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-$('#fullpage').fullpage({
-    menu: '#myMenu',
-    css3: false,
-    scrollingSpeed: 700,
-    autoScrolling: false,//if set True, scroll invisible bug will not come out.
-    scrollOverflow: false,
-    easing: 'easeInQuart',
-    easingcss3: 'ease',
-    loopBottom: false,
-    loopTop: false,
-    loopHorizontal: false,
-    continuousVertical: false,
-    fixedElements:'.header',             
-    touchSensitivity: 5,
-    normalScrollElementTouchThreshold: 15,    
- });
+    $('#fullpage').fullpage();
+    
 });
