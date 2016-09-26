1da6fc4fd4f4fb8158824f108e6187450f471e71
Noemi Ostorga <noemiostorga@gmail.com>
2016-09-26 14:20:29 -0700

    added postion relative to intro paragraph

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
-
+$('#fullpage').fullpage({
+    menu: '#myMenu',
+    css3: false,
+    scrollingSpeed: 700,
+    autoScrolling: false,//if set True, scroll invisible bug will not come out.
+    scrollOverflow: false,
+    easing: 'easeInQuart',
+    easingcss3: 'ease',
+    loopBottom: false,
+    loopTop: false,
+    loopHorizontal: false,
+    continuousVertical: false,
+    fixedElements:'.header',             
+    touchSensitivity: 5,
+    normalScrollElementTouchThreshold: 15,    
+ });
 });
