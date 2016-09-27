ac83279cfc2a81b2afe36b8ab15731f146ddcaba
Noemi Ostorga <noemiostorga@gmail.com>
2016-09-27 16:26:47 -0700

    got rid of fullpage.js and slim.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
 
-    $('#fullpage').fullpage();
+ //    $('#fullpage').fullpage({
+ //    	scrollOverflow: true,
+ //    	fitToSection: true,
+
+ //    	controlArrows: true
+ //    });
     	
    
 
-	$('.fp-scrollable').slimScroll({
-	    height: window.innerHeight+'px',
-	    alwaysVisible: true,
-	    color: '#fff',
-	    size: '10px',
-	    allowPageScroll: true,
-	});
+	// $('.fp-scrollable').slimScroll({
+	//     height: window.innerHeight+'px',
+	//     alwaysVisible: true,
+	//     color: '#fff',
+	//     size: '10px',
+	//     allowPageScroll: true,
+	   
+	// });
      console.log("jeu");
 });
