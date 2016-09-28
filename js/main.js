4aee1035458271011b25f524c74845ad2aae3dda
Noemi Ostorga <noemiostorga@gmail.com>
2016-09-27 19:53:13 -0700

    got rid of fullpage.js and slimscroll

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
 
-    $('#fullpage').fullpage({
-    	scrollOverflow: true,
-    	fitToSection: true,
-    	destroy: true,
-    	controlArrows: true
-    });
-    	
-   
-
-	$('.fp-scrollable').slimScroll({
-	    height: window.innerHeight+'px',
-	    alwaysVisible: true,
-	    color: '#fff',
-	    size: '10px',
-	    allowPageScroll: true,
-	   
-	});
-     console.log("jeu");
+     console.log("hello");
 });
